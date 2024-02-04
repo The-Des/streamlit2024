1a5b49fff7ce0ed95133411dc29af321bb6692b3
The-Des <105569391+The-Des@users.noreply.github.com>
2024-02-03 20:13:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import streamlit as st
-import pandas as py
+import streamlit as st 
+import pandas as pd 
 import numpy as np
 
-st.title('UPC DATA SCIENCE 2024')
+st.title('UPC Data Science 2024')
+st.header('Simulador Ventas')
+n = st.slider("cant. ventas", 5,100, step=1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
+st.line_chart(chart_data)
