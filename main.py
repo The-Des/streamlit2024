a066b43e9348d78b64cd8427b9fd653b78e04b8d
The-Des <105569391+The-Des@users.noreply.github.com>
2024-06-03 09:17:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,23 +13,14 @@ if uploaded_file:
     def extract_entry_time(schedule):
         if schedule in ["OFF", "VAC"]:
             return np.nan
-        time_str = schedule.split(' - ')[0]
-        return pd.to_datetime(time_str, format='%H:%M').time()
+        return pd.to_datetime(schedule.split(' - ')[0], format='%H:%M')
     
     # Aplicar la función a todas las columnas excepto la columna 'Agente'
     entry_times = df.set_index('Agente').applymap(extract_entry_time)
-    
-    # Convertir los tiempos de entrada a datetime64[ns] para futuras operaciones
-    def time_to_datetime64(time_obj):
-        if pd.isna(time_obj):
-            return np.nan
-        return pd.Timestamp.combine(pd.Timestamp.today(), time_obj)
-    
-    entry_times_datetime64 = entry_times.applymap(time_to_datetime64)
-    
+        
     # Mostrar los resultados (opcional, solo para verificación)
-    st.write("Horarios de entrada en formato datetime64[ns]:")
-    st.dataframe(entry_times_datetime64)
+    st.write("Horarios de entrada")
+    st.dataframe(entry_times)
 
     # Guardar el resultado en memoria
-    st.session_state['entry_times'] = entry_times_datetime64
+    st.session_state['entry_times'] = entry_times
